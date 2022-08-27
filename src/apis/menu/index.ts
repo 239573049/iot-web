9bc01e6a6bf7073334347f9a81606f106ac4564f
239573049@qq.com <239573049@qq.com>
2022-08-27 17:25:35 +0800

    新增菜单动态配置

--- src/apis/menu/index.ts
+++ src/apis/menu/index.ts
@@ -6,5 +6,9 @@ class MenuApi {
   getRoleMenu() {
     return request.get(name + 'app/menu/role-menu');
   }
+
+  getMenuTree() {
+    return request.get(name + 'app/menu/menu-tree');
+  }
 }
 export default new MenuApi();
--- src/pages/authority/menus/index.tsx
+++ src/pages/authority/menus/index.tsx
@@ -1,3 +1,29 @@
-export default function IndexPage() {
-  return <div>菜单管理界面</div>;
+import { Component, ReactNode } from 'react';
+import { Tabs } from 'antd';
+import styles from 'index.less';
+import MenuTree from './MenuTree';
+const { TabPane } = Tabs;
+
+export default class Menu extends Component {
+  onChange(key) {
+    console.log(key);
+  }
+
+  render(): ReactNode {
+    return (
+      <div>
+        <Tabs defaultActiveKey="1" onChange={this.onChange}>
+          <TabPane tab="Tab 1" key="1">
+            <MenuTree />
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
+      </div>
+    );
+  }
 }
