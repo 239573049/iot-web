8a4f1c648ca7c44d85cc1cd01f67db3908c01bad
token <239573049@qq.com>
2022-09-10 01:29:09 +0800

    修改

---
+++ src/modules/devices/Device.ts
@@ -0,0 +1,27 @@
+export class DeviceTemplateDto {
+  id: string;
+
+  /**备注 */
+  remark: string;
+
+  /** 状态 */
+  stats: string;
+
+  /**设备名称 */
+  name: string;
+
+  /** 最后活跃时间 */
+  lastTime: Date;
+
+  /** 绑定设备 */
+  userInfoId: string | null;
+
+  /** 设备模板 */
+  deviceTemplateId: string | null;
+
+  /**设备图标 */
+  icon: string;
+
+  /**设备类型 */
+  type: string;
+}
--- src/pages/devices/admin/index.tsx
+++ src/pages/devices/admin/index.tsx
@@ -1,5 +1,5 @@
 import { Component, ReactNode } from 'react';
-import { Table, Button, Input, DatePicker } from 'antd';
+import { Table, Button, Input, DatePicker, Image } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import DeviceModule from '@/modules/DeviceModule';
 import DeviceApi from '@/apis/devices/index';
@@ -40,14 +40,27 @@ const columns: ColumnsType<any> = [
   },
   {
     title: '状态',
-    dataIndex: 'statsName',
-    key: 'statsName',
+    dataIndex: 'stats',
+    key: 'stats',
     width: 150,
   },
   {
-    title: '类型',
-    dataIndex: 'tyleName',
-    key: 'tyleName',
+    title: '最后活跃时间',
+    dataIndex: 'lastTime',
+    key: 'lastTime',
+    width: 150,
+  },
+  {
+    title: '设备图标',
+    dataIndex: 'icon',
+    key: 'icon',
+    width: 150,
+    render: (value) => <Image width={20} src={value.icon}></Image>,
+  },
+  {
+    title: '设备类型',
+    dataIndex: 'type',
+    key: 'type',
     width: 150,
   },
   {
@@ -55,7 +68,14 @@ const columns: ColumnsType<any> = [
     key: 'operation',
     fixed: 'right',
     width: 100,
-    render: () => <a>action</a>,
+    render: (value) => (
+      <div>
+        <Button style={{ margin: '10px' }}>编辑</Button>
+        <Button type="primary" danger>
+          删除
+        </Button>
+      </div>
+    ),
   },
 ];
 
@@ -135,6 +155,13 @@ export default class Admin extends Component<IProps, IState> {
             style={{ width: '80px', margin: '10px' }}
           >
             搜索
+          </Button>
+          <Button
+            onClick={() => this.getDevice()}
+            type="primary"
+            style={{ width: '80px', margin: '10px' }}
+          >
+            新增设备
           </Button>
         </div>
         <Table
